0d25248cbfc5891b273b3ea82e281bae744cc4ce
Ly Vi Cuong <chanhbeck27@gmail.com>
2023-10-12 14:44:30 +0700

    init next

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+	return (
+		<div className='flex justify-center h-full'>
+			<div className='flex flex-col justify-center h-full'>
+				<div>Hello world</div>
+			</div>
+		</div>
+	)
+}
+
+export default Home
